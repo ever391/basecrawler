17c1751ad463102839b4a235835dc3f20484217a
smart <smart.jin@foxmail.com>
2018-06-08 01:30:34 +0800

    update

--- demo/lianjia.py
+++ demo/lianjia.py
@@ -1,4 +1,7 @@
 # coding:utf8
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from basecrawler.basecrawler import BaseCrawler, BeautifulSoup
 import pymysql
